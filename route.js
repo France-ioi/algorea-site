a6600196eac1116676668e84b1a0bc7f64a1de58
Melanie Storup <m.storup@epixode.fr>
2017-12-07 21:44:31 +0100

    initial import

---
+++ route.js
@@ -0,0 +1,114 @@
+var myApp = angular.module('algorea', ['ngRoute']);
+
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+   $routeProvider
+
+   // route for the main page
+   .when('/', {
+      templateUrl : 'pages/presentation.html',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the qualification page
+   /*.when('/qualification', {
+      templateUrl : 'pages/qualification.html',
+      controller  : 'PageCtrl'
+   })*/
+
+   // route for the participation page
+   .when('/participation', {
+      templateUrl : 'pages/participation.html',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the preparation page
+   .when('/preparation', {
+      templateUrl : 'pages/preparation.html',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the awards page
+   .when('/recompenses', {
+      templateUrl : 'pages/awards.html',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the results page
+   .when('/resultats', {
+      templateUrl : 'pages/results.html',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the who are we page
+   .when('/quiSommesNous', {
+      templateUrl : 'pages/whoAreWe.html',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the coordinator page
+   .when('/coordinateur', {
+      templateUrl : 'http://coordinateur.concours-alkindi.fr',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the legal informations page
+   .when('/infosLegales', {
+      templateUrl : 'pages/legalInformation.html',
+      controller  : 'PageCtrl'
+   })
+
+   // route for the instruction page
+   .when('/infosTour2', {
+      templateUrl : 'infos/instructionsTour2.html?v=23',
+      controller  : 'PageCtrl'
+   })
+
+   .otherwise({
+      redirectTo: '/'
+   });
+
+   $locationProvider.hashPrefix('');
+
+}]);
+
+myApp.controller('PageCtrl', function($scope, $location) {
+   $scope.isActive = function (viewLocation) {
+     return viewLocation === $location.path();
+   };
+   $scope.go = function ( path ) {
+     $location.path( path );
+   };
+});
+
+myApp.controller('VideoDisplay', function($scope, $sce) {
+   $scope.displayPlayer = false;
+   $scope.loadVideo = function (item) {
+      $scope.videoUrl = $sce.trustAsResourceUrl(item.currentTarget.getAttribute('data-source'));
+      $scope.displayPlayer = true;
+   };
+});
+
+myApp.controller('TabController', ['$scope', function($scope) {
+   $scope.tab = 1;
+
+   $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+   };
+
+   $scope.isTabSet = function(tabNum){
+      return $scope.tab === tabNum;
+   };
+}]);
+
+myApp.controller('PillController', ['$scope', function($scope) {
+   $scope.pill;
+
+   $scope.setPill = function(newPill){
+      $scope.pill = newPill;
+   };
+
+   $scope.isPillSet = function(pillNum){
+      return $scope.pill === pillNum;
+   };
+}]);
---
+++ tabs.js
@@ -0,0 +1,12 @@
+@Component({
+  selector: 'tabs',
+  template: `
+    <ul>
+      <li>Tab 1</li>
+      <li>Tab 2</li>
+    </ul>
+  `
+})
+export class Tabs {
+
+}
