c8d8eb63e2f405efda396ec82f3f27921b3b5e04
Mathias Hiron <mathias.hiron@gmail.com>
2018-09-11 15:07:30 +0200

    résultats Algoréa 2018

--- route.js
+++ route.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('algorea', ['ngRoute']);
 
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-   var v = "?v=17";
+   var v = "?v=24";
    $routeProvider
 
    // route for the main page
